0b2e81f39f46eceb1f2ce4b9b0f34c9836bcc7e9
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-11-04 05:19:52 +0000

    Install next.js and sanity package and dependencies.

---
+++ sanity/env.js
@@ -0,0 +1,6 @@
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-11-03'
+
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const projectId = 'eq1klnqh';
+export const useCdn = false
