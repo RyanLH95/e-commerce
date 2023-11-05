44d63ef411aecffce0323c4f2db2bd2797269426
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-11-05 13:10:08 +0000

    Create 'components' directory inside 'root' directory and add 'Cart.jsx, Footer.jsx, FooterBanner.jsx, HeroBanner.jsx, Layout.jsx, NavBar.jsx, Product.jsx' files, then export inside index.js file. Import in page.js.

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import { Product, FooterBanner, HeroBanner } from 'components/';
+
 const page = () => {
   return (
-    <div>page</div>
+    <>
+      HeroBanner
+
+    <div className='products-heading'>
+      <h2>Best Selling Trainers</h2>
+      <p>Most Stylish Trainers found right here!</p>
+    </div>
+
+    <div className='products-container'>
+      {['Product 1', 'Product 2'].map(
+        (product) => product)}
+    </div>
+
+      Footer
+    </>
   )
 }
 
